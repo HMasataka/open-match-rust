555da08a9814464fe0bd076f85625d7d57a44fc2
Masataka Hisasue <masataka.hisasue@gmail.com>
2024-10-04 12:56:48 +0900

    Feat request to front

--- game-front/src/main.rs
+++ game-front/src/main.rs
@@ -1,3 +1,19 @@
-fn main() {
-    println!("Hello, world!");
+mod openmatch {
+    tonic::include_proto!("openmatch");
+}
+
+use openmatch::frontend_service_client::FrontendServiceClient;
+use openmatch::CreateTicketRequest;
+
+#[tokio::main]
+async fn main() -> Result<(), Box<dyn std::error::Error>> {
+    let mut client = FrontendServiceClient::connect("http://[::1]:50504").await?;
+
+    let req = tonic::Request::new(CreateTicketRequest { ticket: None });
+
+    let res = client.create_ticket(req).await?;
+
+    println!("{:?}", res);
+
+    Ok(())
 }
