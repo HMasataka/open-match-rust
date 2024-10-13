c4411b3cea1c8082aef5da682fe253365020dbb5
Masataka Hisasue <masataka.hisasue@gmail.com>
2024-10-13 17:15:11 +0900

    Feat split server code

--- match-function/src/main.rs
+++ match-function/src/main.rs
@@ -2,78 +2,17 @@ mod openmatch {
     tonic::include_proto!("openmatch");
 }
 mod err;
+mod server;
 
 use std::net::SocketAddr;
-use std::pin::Pin;
-use std::time::Duration;
 
 use err::MatchFunctionError;
-use openmatch::match_function_server::{MatchFunction, MatchFunctionServer};
-use openmatch::{Match, RunRequest, RunResponse};
-use tokio::sync::mpsc;
-use tokio_stream::wrappers::ReceiverStream;
-use tokio_stream::{Stream, StreamExt};
+use server::make_server;
 use tonic::transport::Server;
-use tonic::{Request, Response, Status};
 use tracing::instrument;
 use tracing_error::ErrorLayer;
 use tracing_spanned::SpanErr;
 use tracing_subscriber::{layer::SubscriberExt, util::SubscriberInitExt, Layer};
-
-#[derive(Default)]
-pub struct MMFServer {}
-
-type RunResult<T> = Result<Response<T>, Status>;
-type ResponseStream = Pin<Box<dyn Stream<Item = Result<RunResponse, Status>> + Send>>;
-
-#[tonic::async_trait]
-impl MatchFunction for MMFServer {
-    type RunStream = ResponseStream;
-
-    #[instrument(skip_all, name = "run", level = "trace")]
-    async fn run(&self, request: Request<RunRequest>) -> RunResult<Self::RunStream> {
-        println!("Got a request from {:?}", request.remote_addr());
-
-        let replies = make_matches();
-
-        let mut stream = Box::pin(tokio_stream::iter(replies).throttle(Duration::from_millis(200)));
-
-        let (tx, rx) = mpsc::channel(128);
-        tokio::spawn(async move {
-            while let Some(item) = stream.next().await {
-                match tx.send(Result::<_, Status>::Ok(item)).await {
-                    Ok(_) => {} // item (server response) was queued to be send to client
-                    Err(_item) => {
-                        // output_stream was build from rx and both are dropped
-                        break;
-                    }
-                }
-            }
-
-            println!("\tclient disconnected");
-        });
-
-        let output_stream = ReceiverStream::new(rx);
-
-        Ok(Response::new(Box::pin(output_stream) as Self::RunStream))
-    }
-}
-
-#[instrument(skip_all, name = "make_matches", level = "trace")]
-fn make_matches() -> Vec<RunResponse> {
-    let mut replies = Vec::new();
-
-    for i in 1..4 {
-        replies.push(RunResponse {
-            proposal: Some(Match {
-                match_id: format!("{}", i),
-                ..Default::default()
-            }),
-        });
-    }
-
-    return replies;
-}
 
 #[instrument(skip_all, name = "initialize_tracing_subscriber", level = "trace")]
 fn initialize_tracing_subscriber() -> Result<(), SpanErr<MatchFunctionError>> {
@@ -98,12 +37,11 @@ async fn main() -> Result<(), SpanErr<MatchFunctionError>> {
     let addr = "[::0]:50502"
         .parse::<SocketAddr>()
         .map_err(MatchFunctionError::FailedAddrParse)?;
-    let server = MMFServer {};
 
     println!("{}", addr);
 
     Server::builder()
-        .add_service(MatchFunctionServer::new(server))
+        .add_service(make_server())
         .serve(addr)
         .await
         .map_err(MatchFunctionError::FailToService)?;
---
+++ match-function/src/server.rs
@@ -0,0 +1,76 @@
+mod openmatch {
+    tonic::include_proto!("openmatch");
+}
+
+use std::pin::Pin;
+use std::time::Duration;
+
+use openmatch::match_function_server::{MatchFunction, MatchFunctionServer};
+use openmatch::{Match, RunRequest, RunResponse};
+use tokio::sync::mpsc;
+use tokio_stream::wrappers::ReceiverStream;
+use tokio_stream::{Stream, StreamExt};
+use tonic::{Request, Response, Status};
+use tracing::instrument;
+
+#[derive(Default)]
+pub struct MMFServer {}
+
+type RunResult<T> = Result<Response<T>, Status>;
+type ResponseStream = Pin<Box<dyn Stream<Item = Result<RunResponse, Status>> + Send>>;
+
+#[tonic::async_trait]
+impl MatchFunction for MMFServer {
+    type RunStream = ResponseStream;
+
+    #[instrument(skip_all, name = "run", level = "trace")]
+    async fn run(&self, request: Request<RunRequest>) -> RunResult<Self::RunStream> {
+        println!("Got a request from {:?}", request.remote_addr());
+
+        let replies = make_matches();
+
+        let mut stream = Box::pin(tokio_stream::iter(replies).throttle(Duration::from_millis(200)));
+
+        let (tx, rx) = mpsc::channel(128);
+        tokio::spawn(async move {
+            while let Some(item) = stream.next().await {
+                match tx.send(Result::<_, Status>::Ok(item)).await {
+                    Ok(_) => {} // item (server response) was queued to be send to client
+                    Err(_item) => {
+                        // output_stream was build from rx and both are dropped
+                        break;
+                    }
+                }
+            }
+
+            println!("\tclient disconnected");
+        });
+
+        let output_stream = ReceiverStream::new(rx);
+
+        Ok(Response::new(Box::pin(output_stream) as Self::RunStream))
+    }
+}
+
+#[instrument(skip_all, name = "make_matches", level = "trace")]
+fn make_matches() -> Vec<RunResponse> {
+    let mut replies = Vec::new();
+
+    for i in 1..4 {
+        replies.push(RunResponse {
+            proposal: Some(Match {
+                match_id: format!("{}", i),
+                ..Default::default()
+            }),
+        });
+    }
+
+    return replies;
+}
+
+#[instrument(skip_all, name = "make_matches", level = "trace")]
+pub fn make_server() -> MatchFunctionServer<MMFServer> {
+    let server = MMFServer {};
+
+    return MatchFunctionServer::new(server);
+}
