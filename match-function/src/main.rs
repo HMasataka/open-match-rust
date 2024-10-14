fca383c94b7e05cd6a1b6e05c181dedbe6fe8db9
Masataka Hisasue <masataka.hisasue@gmail.com>
2024-10-14 13:57:07 +0900

    Refactor split error trace

--- match-function/src/main.rs
+++ match-function/src/main.rs
@@ -3,31 +3,16 @@ mod openmatch {
 }
 mod err;
 mod server;
+mod trace;
 
 use std::net::SocketAddr;
 
 use err::MatchFunctionError;
 use server::make_server;
 use tonic::transport::Server;
+use trace::initialize_tracing_subscriber;
 use tracing::instrument;
-use tracing_error::ErrorLayer;
 use tracing_spanned::SpanErr;
-use tracing_subscriber::{layer::SubscriberExt, util::SubscriberInitExt, Layer};
-
-#[instrument(skip_all, name = "initialize_tracing_subscriber", level = "trace")]
-fn initialize_tracing_subscriber() -> Result<(), SpanErr<MatchFunctionError>> {
-    tracing_subscriber::Registry::default()
-        .with(
-            tracing_subscriber::fmt::layer()
-                .with_target(false)
-                .with_filter(tracing_subscriber::filter::LevelFilter::INFO),
-        )
-        .with(ErrorLayer::default())
-        .try_init()
-        .map_err(MatchFunctionError::InitializeTracingSubscriber)?;
-
-    Ok(())
-}
 
 #[tokio::main]
 #[instrument(skip_all, name = "main", level = "trace")]
---
+++ match-function/src/trace.rs
@@ -0,0 +1,21 @@
+use crate::err::MatchFunctionError;
+
+use tracing::instrument;
+use tracing_error::ErrorLayer;
+use tracing_spanned::SpanErr;
+use tracing_subscriber::{layer::SubscriberExt, util::SubscriberInitExt, Layer};
+
+#[instrument(skip_all, name = "initialize_tracing_subscriber", level = "trace")]
+pub fn initialize_tracing_subscriber() -> Result<(), SpanErr<MatchFunctionError>> {
+    tracing_subscriber::Registry::default()
+        .with(
+            tracing_subscriber::fmt::layer()
+                .with_target(false)
+                .with_filter(tracing_subscriber::filter::LevelFilter::INFO),
+        )
+        .with(ErrorLayer::default())
+        .try_init()
+        .map_err(MatchFunctionError::InitializeTracingSubscriber)?;
+
+    Ok(())
+}
